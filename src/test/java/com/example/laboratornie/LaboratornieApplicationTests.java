94f24de820a4dc48fc6c0dcf4f75fe30b2123a73
m1wqwk <pelagey0306@gmail.com>
2025-10-31 22:50:05 +0300

    first commit

---
+++ src/test/java/com/example/laboratornie/LaboratornieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratornie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratornieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
